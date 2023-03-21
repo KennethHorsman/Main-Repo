af60d36d85a882e00816ad36fbccc0d8b67aa3d6
Kenneth Horsman <125078399+KennethHorsman@users.noreply.github.com>
2023-03-21 01:18:15 +0000

    worked on calc

--- Other/Personal/Calculator.py
+++ Other/Personal/Calculator.py
@@ -1,21 +1,39 @@
-list = []
+import operator
+
+ops = {
+    '+' : operator.add,
+    '-' : operator.sub,
+    '*' : operator.mul,
+    '/' : operator.truediv,  # use operator.div for Python 2
+    '%' : operator.mod,
+    '^' : operator.xor,
+}
+
+operator_input = "+"
+number1 = 4
+number2 = 2
+
+print(ops[operator_input](number1,number2))
+
+
+data_list = []
 user_input = input("Enter equation: ").replace(" ", "")
 for x in user_input:
-    list.append(x)
-print(list)
+    data_list.append(x)
+print(data_list)
 
 
-myIntList = [x for x in list if x.isnumeric()]
-myOperators = [x for x in list if x in ("+-*/")]
+myIntList = [x for x in data_list if x.isnumeric()]
+myOperators = [x for x in data_list if x in ("+-*/")]
 operatorIndex = []
 for x in myOperators:
-    operatorIndexes = list.index(x)
+    operatorIndexes = data_list.index(x)
     operatorIndex.append(operatorIndexes)
-    list[operatorIndexes] = F"operator: {x}"
+    data_list[operatorIndexes] = F"operator: {x}"
 
-print("original list: ", list)
-print("int list: ", myIntList)
-print("ops list: ", myOperators)
-print("ops index list: ", operatorIndex)
+print("original data_list: ", data_list)
+print("int data_list: ", myIntList)
+print("ops data_list: ", myOperators)
+print("ops index data_list: ", operatorIndex)
 
-# Find indexes of operators, join elements in list around operator. Use Try Except to see if element calculation is possible?
+# Find indexes of operators, join elements in data_list around operator. Use Try Except to see if element calculation is possible?
--- test.py
+++ test.py
@@ -1,6 +1,2 @@
-
-dict = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five"}
-print(list(dict.values()))
-print(list(dict.keys()))
-print(list(dict.items()))
-print(list(dict.items())[0][1], list(dict.items())[4][0])
+data_list = ['+']
+print(type(data_list[0]))
