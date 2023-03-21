f057c1d083c1d140cf95378c321aa901146d1024
Kenneth Horsman <125078399+KennethHorsman@users.noreply.github.com>
2023-03-21 16:25:42 +0000

    clac

--- Other/Personal/Calculator.py
+++ Other/Personal/Calculator.py
@@ -15,24 +15,32 @@ ops = {
 # print(ops[operator_input](number1,number2))
 
 
-data_list = []
 user_input = input("Enter equation: ").replace(" ", "")
+
+myOperators = [x for x in user_input if x in ("+-*/")]
+
+new_list = []
 for x in user_input:
-    data_list.append(x)
-print(data_list)
+    if x.isnumeric():
+        new_list.append(int(x))
+    elif x in myOperators:
+        new_list.append(x)
+    else:
+        pass
 
-
-myIntList = [x for x in data_list if x.isnumeric()]
-myOperators = [x for x in data_list if x in ("+-*/")]
+new_list_copy = new_list[0:]
 operatorIndex = []
 for x in myOperators:
-    operatorIndexes = data_list.index(x)
+    operatorIndexes = new_list_copy.index(x)
     operatorIndex.append(operatorIndexes)
-    data_list[operatorIndexes] = F"operator: {x}"
+    new_list_copy[operatorIndexes] = f"ops: {x}"
+    
 
-print("original data_list: ", data_list)
-print("int data_list: ", myIntList)
-print("ops data_list: ", myOperators)
-print("ops index data_list: ", operatorIndex)
+
+
+    
+print("new list: ", new_list)
+print("ops: ", myOperators)
+print("ops index: ", operatorIndex)
 
 # Find indexes of operators, join elements in data_list around operator. Use Try Except to see if element calculation is possible?
