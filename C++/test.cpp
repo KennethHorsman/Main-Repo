ac1418f106901c8a5be46118887fef5d26b2dab2
Kenneth Horsman <125078399+KennethHorsman@users.noreply.github.com>
2023-09-09 04:23:13 +0000

    test

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,8 +1,32 @@
-// Your First C++ Program
+// Assigning a grade letter (A, B, C, D, F) based on the numerical score a student receives.
 
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello World!\n";
 
+   int score;
+   int scoreDivided;
+   bool passed = false;
+
+   cout << "Enter your score: \n";
+   cin >> score;
+
+   scoreDivided = score / 50;
+
+   switch(scoreDivided) {
+
+      case(1):
+      case(2):
+         passed = true;
+
+   if (passed) {
+      cout << "You passed!\n";
+   }
+   else {
+      cout << "You failed!\n";
+   }
+   }
+
+   return 0;
 }
