e815f1979662827c6fa2db545a5da2c324a5b3f2
Kenneth Horsman <125078399+KennethHorsman@users.noreply.github.com>
2023-12-04 05:35:50 +0000

    a

---
+++ C++/stepstomiles.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <stdexcept>
+
+using namespace std;
+
+double StepsToMiles(int steps) {
+    if (steps < 0) {
+        throw runtime_error("Exception: Negative step count entered.");
+    }
+    double miles = static_cast<double>(steps) / 2000.0;
+    return miles;
+}
+
+int main() {
+    int steps;
+    cin >> steps;
+
+    try {
+        double miles = StepsToMiles(steps);
+        cout << fixed << setprecision(2);
+        cout << miles << endl;
+    } catch (runtime_error &e) {
+        cout << e.what() << endl;
+    }
+
+    return 0;
+}a
