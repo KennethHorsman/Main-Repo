70c5d5546a5194bfabd2b748317f611b1b87f305
Kenneth Horsman <125078399+KennethHorsman@users.noreply.github.com>
2024-01-27 05:57:25 +0000

    h

--- C++/Labs/HighwayNumbers.cpp
+++ C++/Labs/HighwayNumbers.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 bool IsEven(string highwayNumber) {
-    return stoi(highwayNumber) % 2 == 0;
+    return stoi(highwayNumber) % 2 == 0; // temporarily converts highwaynumber into an int & checks if it evenly divides
 }
 
 int main() {
@@ -10,12 +10,12 @@ int main() {
    
    cin >> highwayNumber;
 
-   // If invalid highway number
+   // If invalid highway number (equal to "0", has a "00", or is more than 3 digits)
    if ((highwayNumber.length() == 1 && highwayNumber == "0") || highwayNumber.find("00") != string::npos || highwayNumber.length() > 3) {
       cout << highwayNumber << " is not a valid interstate highway number." << endl;
    }
    
-   // If auxiliary highway
+   // If auxiliary highway (3 digits), get service number (two rightmost digits - unless the digit at index 1 is a 0, then the rightmost digit only)
    else if (highwayNumber.length() == 3) {
       serviceNumber = (highwayNumber[1] != '0' ? highwayNumber.substr(1, 2) : highwayNumber.substr(2, 2));
       cout << "I-" << highwayNumber << " is auxiliary, serving I-" << serviceNumber;
