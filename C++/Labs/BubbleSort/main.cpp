03c36f1ebe83d5a6714217016c476605f3fcd5f1
Kenneth Horsman <125078399+KennethHorsman@users.noreply.github.com>
2024-02-09 19:00:18 +0000

    v

--- C++/Labs/BubbleSort/main.cpp
+++ C++/Labs/BubbleSort/main.cpp
@@ -1,7 +1,7 @@
 //============================================================================
-// Name        : Bubble Sort (Main)
+// Name        : Bubble Sort (All hares)
 // Author      : Kenneth Horsman
-// Version     : 1
+// Version     : 2
 // Copyright   : Your copyright notice
 // Description : Basic searching examples
 //============================================================================
@@ -10,53 +10,57 @@
 #include <iostream>
 #include <time.h>
 
-#include "SortTester.cpp" // Default included SortTester.h instead, which caused an error when using VSCode but not in ZyBooks
+#include "SortTester.cpp"
 
 using namespace std;
 
-bool singleBubblePass(SortTester &tester, unsigned int size, unsigned int passNum) {
+bool singleBubblePass(SortTester &tester, unsigned int lowEnd, unsigned int highEnd, unsigned int passNum) {
     bool sorted = true;
-    bool lowToHigh = passNum % 2 == 1; // Determine sorting direction
-    int newLastSwapIndex = 0;
+    bool lowToHigh = passNum % 2 == 1; // Determine sorting direction based on even/odd pass
 
     if (lowToHigh) {
-        for (unsigned int index = 0; index < size - 1; ++index) {
-            if (tester.compare(index, index + 1) > 0) {
-                tester.swap(index, index + 1);
-                sorted = false;
-                newLastSwapIndex = index;  // Update the last swap index
+        bool swapped = false; // Flag to indicate if a swap occurred in this pass
+        for (unsigned int index = lowEnd; index < highEnd - 1; ++index) { // for each index in array from specified range
+            if (tester.compare(index, index + 1) > 0) { // if curr value greater than the next
+                tester.swap(index, index + 1); // swap them
+                swapped = true; // change flag to indicate a swap occurred
             }
         }
+        sorted = !swapped; // If no swap occurred, the list is sorted
     } else {
-        for (unsigned int index = size - 1; index > 0; --index) {
+        bool swapped = false; 
+        for (unsigned int index = highEnd - 1; index > lowEnd; --index) {
             if (tester.compare(index, index - 1) < 0) {
                 tester.swap(index, index - 1);
-                sorted = false;
-                newLastSwapIndex = index;  // Update the last swap index
+                swapped = true;
             }
         }
+        sorted = !swapped;
     }
 
-    // Check if the last element is in its correct position
-    if (tester.compare(size - 1, size - 2) >= 0) {
-        size = newLastSwapIndex + 1;  // Reduce the range for the next pass
-    }
-
-    return sorted;  // Return true if the pass completed without any swaps
+    return sorted; // Return true if the pass completed without any swaps
 }
 
 
 int main() {
-	unsigned int size = 10;
-	SortTester tester = SortTester(size);
-	cout<<"Unsorted"<<endl;
-	tester.print();
-	bool sorted = false;
-	unsigned int numPasses = 0;
-	while (not sorted) {
-		sorted = true;
-		numPasses++;
-		sorted = singleBubblePass(tester, size, numPasses); //this is the function you are defining
-   }
-   tester.print();
+    unsigned int size = 10;
+    SortTester tester = SortTester(size);
+    cout << "Unsorted" << endl;
+    tester.print();
+    bool sorted = false;
+    unsigned int numPasses = 0;
+    unsigned int lowEnd = 0;
+    unsigned int highEnd = size;
+    while (!sorted) {
+        sorted = true;
+        numPasses++;
+        sorted = singleBubblePass(tester, lowEnd, highEnd, numPasses); // Perform a single pass
+        if (numPasses % 2 == 0) {
+            highEnd--; // Remove last element on even pass
+        } else {
+            lowEnd++; // Remove first element on odd pass
+        }
+    }
+
+    tester.print();
 }
