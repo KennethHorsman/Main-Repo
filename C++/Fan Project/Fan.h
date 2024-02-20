b3dd43eb0643fe1c8cba266570fd6bec1661b134
Kenneth Horsman <125078399+KennethHorsman@users.noreply.github.com>
2024-02-20 05:51:21 +0000

    a

--- C++/Fan Project/Fan.h
+++ C++/Fan Project/Fan.h
@@ -7,8 +7,8 @@ class Fan {
     public:
         Fan(); 
         void setSpeed(int newSpeed); 
-        int getSpeed() const; 
-        void setState(bool newOn); 
+        int getSpeed() const; // using const indicates that data is not being modified
+        void setState(bool state); 
         bool getState() const; 
         void setRadius(double newRadius); 
         double getRadius() const; 
@@ -16,7 +16,7 @@ class Fan {
         string getColor() const; 
         void displayFan() const; 
         
-        static const int SLOW;
+        static const int SLOW; // static indicates that this data member belongs to the class rather than the instance
         static const int MEDIUM;
         static const int FAST;
     
