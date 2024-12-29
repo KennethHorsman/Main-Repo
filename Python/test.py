33cb2043034e8cc3fe27da5202fe434303a42001
Kenneth Horsman <125078399+KennethHorsman@users.noreply.github.com>
2024-12-29 16:56:53 +0000

    a

--- Python/test.py
+++ Python/test.py
@@ -10,17 +10,17 @@
 # }
 
 # import re
- 
+
 # # initializing string
 # test_str = "45 + 98-10"
- 
+
 # # printing original string
 # print("The original string is : " + test_str)
- 
+
 # # Expression evaluation in String
 # # Using regex + map() + sum()
 # res = sum(map(int, re.findall(r'[+-]?\d+', test_str)))
- 
+
 # # printing result
 # print("The evaluated result is : " + str(res))
 
