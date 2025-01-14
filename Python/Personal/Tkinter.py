a89979b65b43e88570003b10560ad22f52c7f045
Kenneth Horsman <125078399+KennethHorsman@users.noreply.github.com>
2025-01-14 04:23:05 +0000

    tkinter test

---
+++ Python/Personal/Tkinter.py
@@ -0,0 +1,15 @@
+'''
+to create V-Env folder: python3 -m tkinter_venv
+to active V-Env: source ./tkinter_venv/bin/activate
+install tkinter: pip3 install tk
+
+Simple layout:
+
+import tkinter
+
+app = tkinter.Tk()
+--widgets here--
+app.mainloop()
+
+
+'''
