c833bab8ecdd9143f190e555fa2fb30f589017e9
Kenneth Horsman <125078399+KennethHorsman@users.noreply.github.com>
2025-05-06 21:40:04 +0000

    5/6

---
+++ Python/UofC/CPSC217 Excercises/Excercise5.py
@@ -0,0 +1,28 @@
+'''
+Write a program that reads a collection of values from the user. After all of the values have been read,
+display all of the values in the opposite order from which they were entered, with one value appearing
+on each line. The user will indicate that no further values will be entered by entering a blank line.
+'''
+
+def main():
+    list_of_values = []
+    getting_values = True
+
+    print('This program...')
+
+    while getting_values:
+        value = input("Enter value (or nothing to quit): ")
+        
+        if value:
+            list_of_values.append(value)
+        else:
+            getting_values = False
+    
+    list_of_values.reverse()
+    print("Values listed in reverse order:")
+    for value in list_of_values:
+        print(value)
+
+if __name__ == "__main__":
+    main()
+
---
+++ Python/UofC/CPSC217 Excercises/Excercise6.py
@@ -0,0 +1,13 @@
+"""
+Write a program that reads a postal code from the user and displays the name of the province in which
+that postal code resides. Your program cannot include any if statements, loops or lists. As a result,
+you will almost certainly want to use a dictionary to solve this problem.
+Your program does not need to do any error checking. The user will always enter a 6-character postal
+code that begins with one of the letters indicated in the previous table.
+"""
+
+def main():
+    print('')
+
+if __name__=="__main__":
+    main()
