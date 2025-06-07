36164fc7467fd4270a6f97c71f9a2b184644c06c
Kenneth Horsman <kenneth.h1195@gmail.com>
2025-06-06 23:36:08 -0600

    e7

--- School/UofC/CPSC217/Excercises/Exercise7.py
+++ School/UofC/CPSC217/Excercises/Exercise7.py
@@ -2,12 +2,16 @@
 
 file_name = input("Enter the name of a file: ")
 
-file_contents = open(file_name)
+file_contents = open(file_name).readlines()
 
 num_commas = 0
+num_lines = 0
 
-for char in file_contents:
-    if char == ",":
-        num_commas += 1
+for line in file_contents:
+    for char in line:
+        if char == ",":
+            num_commas += 1
+            
+    num_lines += 1
 
-print(f"That file contains {num_commas + 1} values.")
+print(f"That file contains {num_commas + num_lines} values.") # On each line, there's 1 more value than there is commas, so I'm adding a 1 per line
