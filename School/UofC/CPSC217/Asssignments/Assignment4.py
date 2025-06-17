e4c7c69444ef39d3b5293536c2af4196a419297b
Kenneth Horsman <kenneth.h1195@gmail.com>
2025-06-16 22:21:18 -0600

    a

--- School/UofC/CPSC217/Asssignments/Assignment4.py
+++ School/UofC/CPSC217/Asssignments/Assignment4.py
@@ -13,14 +13,24 @@ ENCOUNTERED PROBLEM:
 
 """
 
+SINGLE_HEX_MAX = 15
+DOUBLE_HEX_MAX = 255
+SINGLE_HEX_CONVERSION = "%x"
+DOUBLE_HEX_CONVERSION = "%02x"
+BIAS = 8
+DIFFERENCE_MIN = -8
+DIFFERENCE_MAX = 7
+SEEN_COLORS_MAX = 256
+LINE_MAX = 80
+
 def main():
     output_name = "output.okti"
 
     if len(sys.argv) == 2:
-        input_file = sys.argv[1] # Second line of the arguments, the first being the script name
+        input_file = sys.argv[1] # Second argument, the first being the script name
     elif len(sys.argv) > 2:
         print("Error: Too many arguments provided.")
-        quit() # I could loop this error, and the below two errors, but the instructions say to quit.
+        quit()
     else:
         input_file = input("Enter a file name (including extension): ")
 
@@ -57,6 +67,7 @@ def encodeOKTI(image, fname):
     seen_colors = [(0,0,0)]
     prev_pixel = (0,0,0)
     x = 0; y = 0
+    current_line = ""
 
     with open(fname, "w") as file: # Automatically closes when done
         file.write("okti\n")
@@ -70,31 +81,38 @@ def encodeOKTI(image, fname):
 
                 match method:
                     case 1:
-                        newline, skips = copyColor(pixel, x, y, image, WIDTH, HEIGHT)
-                        file.write(f"{newline}\n")
+                        pixel_to_write, skips = copyColor(pixel, x, y, image, WIDTH, HEIGHT)
                         for _ in range(skips):
                             x += 1
-
+                            if x == WIDTH:
+                                y += 1
+                                x = 0
                     case 2:
-                        newline = seenColor(pixel, seen_colors)
-                        file.write(f"{newline}\n")
+                        pixel_to_write = seenColor(pixel, seen_colors)
                     case 3:
-                        newline = smallDifference(pixel, prev_pixel)
-                        file.write(f"{newline}\n")
+                        pixel_to_write = smallDifference(pixel, prev_pixel)
                     case 4:
-                        newline = fullRGB(pixel)
-                        file.write(f"{newline}\n")
-                        
+                        pixel_to_write = fullRGB(pixel)
+
+                if len((current_line + pixel_to_write)) < LINE_MAX:    
+                    current_line += pixel_to_write
+                else:
+                    file.write(current_line + "\n")
+                    current_line = f"{pixel_to_write}"
+
                 if pixel not in seen_colors:
                     seen_colors.insert(0, pixel)
 
-                if len(seen_colors) > 256:
+                if len(seen_colors) > SEEN_COLORS_MAX:
                     seen_colors.pop()
 
                 prev_pixel = pixel
                 x += 1
             y += 1
             x = 0
+    
+        if current_line: # The last line gets skipped without adding this
+            file.write(current_line + "\n")
 
 """
 Determines which of the four methods to use to encode a specific pixel.
@@ -106,8 +124,8 @@ Parameters:
 
 Returns:
     1 - Indicates a copy of the previous pixel.
-    2 - Indicates a previously seen pixel.
-    3 - Indicates a similar pixel to the previous one.
+    2 - Indicates an already seen pixel that is not a copy of the previous pixel.
+    3 - Indicates a similar pixel to the previous one, which has not been seen before.
     4 - Indicates none of the above are applicable.
 """
 def determineMethod(pixel, seen_colors, prev_pixel):
@@ -118,9 +136,9 @@ def determineMethod(pixel, seen_colors, prev_pixel):
         return 1
     elif pixel in seen_colors:
         return 2
-    elif (-8 <= (red - prev_red) <= 7 and # because f - 8 = 7, but 0 - 8 = -8
-        -8 <= (green - prev_green) <= 7 and
-        -8 <= (blue - prev_blue) <= 7):
+    elif (DIFFERENCE_MIN <= (red - prev_red) <= DIFFERENCE_MAX and # f - 8 = 7, but 0 - 8 = -8
+        DIFFERENCE_MIN <= (green - prev_green) <= DIFFERENCE_MAX and
+        DIFFERENCE_MIN <= (blue - prev_blue) <= DIFFERENCE_MAX):
         return 3
     else:
         return 4
@@ -132,17 +150,17 @@ Parameters:
     pixel - The current pixel to test.
 
 Returns:
-    newline - The fully translated line to be inserted into the OKTI file.
+    pixel_to_write - The fully translated line to be inserted into the OKTI file.
 """
 def fullRGB(pixel):
     red, green, blue = pixel
 
-    red_hex = "%02x" % red
-    green_hex = "%02x" % green
-    blue_hex = "%02x" % blue
+    red_hex = DOUBLE_HEX_CONVERSION % red
+    green_hex = DOUBLE_HEX_CONVERSION % green
+    blue_hex = DOUBLE_HEX_CONVERSION % blue
 
-    newline = f"p{red_hex}{green_hex}{blue_hex}"
-    return newline
+    pixel_to_write = f"p{red_hex}{green_hex}{blue_hex}"
+    return pixel_to_write
 
 """
 Translates the current pixel and the previous pixel into a difference of RGB values.
@@ -152,18 +170,18 @@ Parameters:
     prev_pixel - The previous pixel.
 
 Returns:
-    newline - The fully translated line to be inserted into the OKTI file.
+    pixel_to_write - The fully translated line to be inserted into the OKTI file.
 """
 def smallDifference(pixel, prev_pixel):
     red, green, blue = pixel
     prev_red, prev_green, prev_blue = prev_pixel
 
-    diff_red = "%x" % ((red - prev_red) + 8) 
-    diff_green = "%x" % ((green - prev_green) + 8) 
-    diff_blue = "%x" % ((blue - prev_blue) + 8)
+    diff_red = SINGLE_HEX_CONVERSION % ((red - prev_red) + BIAS)
+    diff_green = SINGLE_HEX_CONVERSION % ((green - prev_green) + BIAS) 
+    diff_blue = SINGLE_HEX_CONVERSION % ((blue - prev_blue) + BIAS)
 
-    newline = f"d{diff_red}{diff_green}{diff_blue}"
-    return newline
+    pixel_to_write = f"d{diff_red}{diff_green}{diff_blue}"
+    return pixel_to_write
 
 """
 Translates the current pixel into a full set of RGB values for the OKTI file.
@@ -173,17 +191,17 @@ Parameters:
     seen_colors - The list of previously seen pixels.
 
 Returns:
-    newline - The fully translated line to be inserted into the OKTI file.
+    pixel_to_write - The fully translated line to be inserted into the OKTI file.
 """
 def seenColor(pixel, seen_colors):
     index = seen_colors.index(pixel)
 
-    if index <= 15:
-        newline = f"i{"%x" % index}"
+    if index <= SINGLE_HEX_MAX:
+        pixel_to_write = f"i{SINGLE_HEX_CONVERSION % index}"
     else:
-        newline = f"I{"%02x" % index}"    
+        pixel_to_write = f"I{DOUBLE_HEX_CONVERSION % index}"    
     
-    return newline
+    return pixel_to_write
 
 """
 Translates the current pixel into a full set of RGB values for the OKTI file.
@@ -197,7 +215,7 @@ Parameters:
     height - The height of the image.
 
 Returns:
-    newline - The fully translated line to be inserted into the OKTI file.
+    pixel_to_write - The fully translated line to be inserted into the OKTI file.
     skips - The number of x coordinates to skip, based on the number of identical pixels.
 """
 def copyColor(pixel, x, y, image, width, height):
@@ -205,7 +223,7 @@ def copyColor(pixel, x, y, image, width, height):
     temp_x = x; temp_y = y
     get_next = True
 
-    while get_next:
+    while get_next and run < DOUBLE_HEX_MAX:
         temp_x += 1
 
         if temp_x == width:
@@ -214,19 +232,18 @@ def copyColor(pixel, x, y, image, width, height):
 
         if temp_y == height:
             get_next = False
-        
-        if getPixel(image, temp_x, temp_y) == pixel:
+        elif getPixel(image, temp_x, temp_y) == pixel:
             run += 1
         else:
             get_next = False
 
-    if run <= 15:
-        newline = f"r{"%x" % run}"
+    if run <= SINGLE_HEX_MAX:
+        pixel_to_write = f"r{SINGLE_HEX_CONVERSION % run}"
     else:
-        newline = f"R{"%02x" % run}"
+        pixel_to_write = f"R{DOUBLE_HEX_CONVERSION % run}"
     
-    skips = run - 1
-    return newline, skips
+    skips = run - 1 # Subtracting 1 to account for the current line being written (5 runs = 1 written line + 4 skipped lines)
+    return pixel_to_write, skips
 
 if __name__ == "__main__":
     main()
